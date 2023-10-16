2a07c787d713dd5bd982a9685378f64671775f15
Richard Dewan <richarddewan@hotmail.com>
2023-10-16 08:19:54 +0700

    Component Structure & Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { Button, CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom/';
 
+import { Actors, MovieInformation, Movies, NavBar, Profile} from './'
+
+
 const App = () => (
   <div>
   <CssBaseline />
-  <h1>Hello World</h1>
-  <Button variant='contained'>Click</Button>
+  {/* show navbar in all the route */}
+  <NavBar />
+  {/* main part of our app */}
+  <main>
+  {/* switch route and only 1 route is visiable at a time */}
+    <Switch>
+      <Route exact path="/">
+        <Movies />
+      </Route>     
+      <Route exact path="/movie/:id">
+        <MovieInformation />
+      </Route>
+      <Route exact path="/actors/:id">
+        <Actors />
+      </Route>
+      <Route exact path="/profile/:id">
+        <Profile />
+      </Route>
+    </Switch>
+  </main>
   </div>
 );
 
