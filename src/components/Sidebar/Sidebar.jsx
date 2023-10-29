1ca8090d37809cb322e6e16004687a86744998f3
Richard Dewan <richarddewan@hotmail.com>
2023-10-29 09:16:47 +0700

    implement genres api call

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,27 +4,16 @@ import { Divider, List, ListItem, ListItemText, ListSubheader, ButtonBase, ListI
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
+import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
+import genresIcons from '../../assets/genres';
 
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();
   const classes = useStyles();
   const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
   const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
-  const genres = [
-    {
-      label: 'Comedy', value: 'comedy',
-    },
-    {
-      label: 'Action', value: 'action',
-    },
-    {
-      label: 'Horror', value: 'horror',
-    },
-    {
-      label: 'Animation', value: 'animation',
-    },
-  ];
+  const { data, isFetching } = useGetGenresQuery(); 
   const categories = [
     {
       label: 'Popular', value: 'popular',
@@ -53,13 +42,13 @@ const Sidebar = ({ setMobileOpen }) => {
             categories.map(({ label, value }) => (
               <Link key={value} className={classes.links} to="/">
                 <ListItem onClick={() => {}}>
-                  {/* <ListItemIcon>
-                    <img
-                      src={redLogo}
+                  <ListItemIcon>
+                    {/* <img
+                      src={genresIcons[name.toLowerCase()]}
                       className={classes.genreImages}
                       height={30}
-                    />
-                  </ListItemIcon> */}
+                    /> */}
+                  </ListItemIcon>
                   <ListItemText primary={label} />
                 </ListItem>
               </Link>
@@ -70,20 +59,24 @@ const Sidebar = ({ setMobileOpen }) => {
       <List>
         <ListSubheader>Genres</ListSubheader>
         {
-            genres.map(({ label, value }) => (
-              <Link key={value} className={classes.links} to="/">
-                <ListItem onClick={() => {}}>
-                  {/* <ListItemIcon>
-                    <img
-                      src={redLogo}
-                      className={classes.genreImages}
-                      height={30}
-                    />
-                  </ListItemIcon> */}
-                  <ListItemText primary={label} />
-                </ListItem>
-              </Link>
-            ))
+          isFetching ? (
+            <Box display="flex" justifyContent="center">
+              <CircularProgress />
+            </Box>
+          ) : data.genres.map(({ name, id }) => (
+            <Link key={name} className={classes.links} to="/">
+              <ListItem onClick={() => {}}>
+                <ListItemIcon>
+                  <img
+                    src={genresIcons[name.toLowerCase()]}
+                    className={classes.genreImages}
+                    height={30}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
+            </Link>
+          ))
         }
       </List>
     </>
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,7 +7,11 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    // get movies by [type]
+    //* get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+    //* get movies by [type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
@@ -15,6 +19,7 @@ export const tmdbApi = createApi({
 });
 
 export const {
+  useGetGenresQuery,
   useGetMoviesQuery,
 } = tmdbApi;
 
